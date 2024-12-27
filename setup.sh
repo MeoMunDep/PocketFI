c8fb8682665071e4b02310fa2a0db8058bb7367e
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-27 16:10:48 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,10 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "delayEachAccount": [5, 8]
 }
 EOL
 }
@@ -55,7 +49,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Pocketfi BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +109,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd pocketfi && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
